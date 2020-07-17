33aa32aaf8815c6803cb7da46c469ae43deb9cd0
Josh Wright <wright.jjw@gmail.com>
2020-07-16 20:17:29 -0500

    middleware

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ var cors = require("cors");
 var app = express();
 app.use(cors());
 
+app.use('/*', function(req,res,next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    next();
+});
+
 app.get("/db/event/home", (request, response) => {
     let db = new sqlite.Database("data.db");
     db.all("SELECT Event.id AS eid, Event.name AS ename, Event.desc AS edesc, \
